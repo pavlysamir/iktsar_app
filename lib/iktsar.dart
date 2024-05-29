ad2c6cc3a01eda431fc67e5d27e3aa37515a6770
pavlysamir <bavlysamir33@gmail.com>
2024-05-29 15:21:25 +0300

    add structure to app

---
+++ lib/iktsar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IktsarApp extends StatelessWidget {
+  const IktsarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:iktsar_app/iktsar.dart';
+
+void main() {
+  runApp(const IktsarApp());
+}
